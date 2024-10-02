c43638d6e70d65d666708b23ba791d34fd9344eb
motya <motayzz32@gmail.com>
2024-10-02 13:36:54 +0300

    feat:     ConfigReader implemented

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -1,11 +1,28 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IConfigReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ConfigReader implements IConfigReader {
 
   @Override
-  public String loadConfig(String configPath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.json
+  public String loadConfig(String configPath) throws FilenameShouldNotBeEmptyException {
+    if (configPath == null || configPath.isEmpty()) {
+      throw new FilenameShouldNotBeEmptyException("Filename should not be empty");
+    }
+
+    String content = "";
+    Path path = Paths.get(configPath);
+    try {
+      content = Files.readString(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return content;
   }
 }
