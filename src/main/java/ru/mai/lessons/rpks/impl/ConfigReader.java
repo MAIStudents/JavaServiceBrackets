2c0e0171e0df8deacf5977e0ee239866002f8c5f
Ka1amboor <aleksandra.lisnyak@mail.ru>
2024-10-04 22:55:47 +0300

    done

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -1,11 +1,32 @@
 package ru.mai.lessons.rpks.impl;
 
-import ru.mai.lessons.rpks.IConfigReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
-public class ConfigReader implements IConfigReader {
+import ru.mai.lessons.rpks.IConfigReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
+
+import java.io.IOException;
+
+public class ConfigReader implements IConfigReader
+{
 
   @Override
-  public String loadConfig(String configPath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.json
+  public String loadConfig(String configPath) throws FilenameShouldNotBeEmptyException {
+    if(configPath == null || configPath.isEmpty())
+    {
+      throw new FilenameShouldNotBeEmptyException("filePath is empty or file is invalid");
+    }
+    try
+    {
+      Path path = Paths.get(configPath);
+      return Files.readString(path);
+    }
+    catch (IOException e)
+    {
+      throw new FilenameShouldNotBeEmptyException("error file reading");
+    }
+
   }
 }
--- src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
@@ -1,12 +1,38 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IFileReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
-public class FileReader implements IFileReader {
+public class FileReader implements IFileReader
+{
   @Override
-  public List<String> loadContent(String filePath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.txt
+  public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException
+  {
+    if (filePath == null || filePath.isEmpty())
+    {
+      throw new FilenameShouldNotBeEmptyException("file is empty or invalid filepath");
+    }
+    try
+    {
+      Path path = Paths.get(filePath);
+      return Files.readAllLines(path);
+    }
+    catch(IOException ex)
+    {
+      throw new RuntimeException("Error reading file", ex);
+    }
+
   }
+
+
+
 }
