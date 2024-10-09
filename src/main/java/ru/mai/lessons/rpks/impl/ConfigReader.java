ecc857cb08399e82821ea3c18f764388a7fe236b
cucurbitapepo <anedav@yandex.ru>
2024-10-09 23:43:59 +0300

    lab 1 done. all tests passed.

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -1,11 +1,36 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IConfigReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
+
+import java.util.*;
+import java.io.File;
 
 public class ConfigReader implements IConfigReader {
 
   @Override
-  public String loadConfig(String configPath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.json
+  public String loadConfig(String configPath) throws FilenameShouldNotBeEmptyException, RuntimeException {
+
+    if (configPath == null || configPath.isEmpty()) {
+      throw new FilenameShouldNotBeEmptyException("Incorrect file path.");
+    }
+
+    List<String> result = new ArrayList<>();
+    try {
+      File file = new File(configPath);
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        result.add(scanner.nextLine());
+      }
+
+      if (result.isEmpty()) {
+        throw new RuntimeException("File should not be empty: " + configPath);
+      }
+
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
+
+    return String.join(System.lineSeparator(), result) + System.lineSeparator();
   }
 }
--- src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
@@ -1,12 +1,31 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IFileReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
 
-import java.util.List;
+import java.util.*;
+import java.io.File;
 
 public class FileReader implements IFileReader {
   @Override
-  public List<String> loadContent(String filePath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.txt
+  public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException {
+    if(filePath == null || filePath.isEmpty()){
+      throw new FilenameShouldNotBeEmptyException("Empty filename given");
+    }
+
+    List<String> result = new ArrayList<>();
+
+    try{
+      File file = new File(filePath);
+      Scanner scanner = new Scanner(file);
+      while(scanner.hasNextLine()){
+        result.add(scanner.nextLine());
+      }
+    }
+    catch(Exception e)
+    {
+      throw new RuntimeException(e.getMessage());
+    }
+    return result; // написать код загрузки конфигураций сервиса проверки скобок из файла *.txt
   }
 }
