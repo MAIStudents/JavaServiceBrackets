4a14055bad2a7eb583a5f9dee6ba537ee81aa069
motya <motyazz32@gmail.com>
2024-10-22 17:41:26 +0300

    code style

--- src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
@@ -10,19 +10,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader implements IFileReader {
-  @Override
-  public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException{
-    if (filePath == null || filePath.isEmpty()) {
-      throw new FilenameShouldNotBeEmptyException("Config path shouldn't be null nor empty");
-    }
+	@Override
+	public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException {
+		if (filePath == null || filePath.isEmpty()) {
+			throw new FilenameShouldNotBeEmptyException("Config path shouldn't be null nor empty");
+		}
 
-    List<String> listString;
-    try {
-      listString = new ArrayList<>(Files.readAllLines(Paths.get(filePath)));
-    } catch (IOException e) {
-      e.printStackTrace();
-      return new ArrayList<>();
-    }
-    return listString;
-  }
+		List<String> listString;
+		try {
+			listString = new ArrayList<>(Files.readAllLines(Paths.get(filePath)));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+		return listString;
+	}
 }
