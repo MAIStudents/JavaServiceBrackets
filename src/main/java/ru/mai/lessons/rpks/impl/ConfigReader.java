1912f23a9383f0c307c1d8d3ea0588cb0b6bd815
Astakhova Anastasia <asanastasia0201@gmail.com>
2024-11-04 00:13:13 +0300

    first_commit!

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -1,11 +1,17 @@
 package ru.mai.lessons.rpks.impl;
 
+import java.io.FileInputStream;
+
 import ru.mai.lessons.rpks.IConfigReader;
 
 public class ConfigReader implements IConfigReader {
 
   @Override
   public String loadConfig(String configPath) {
+    // if (configPath.isEmpty()) {
+    
+    // }
+
     return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.json
   }
 }
