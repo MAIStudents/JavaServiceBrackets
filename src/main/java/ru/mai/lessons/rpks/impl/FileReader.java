a863f0783d9a74d675316e5c124aeacba5984fd3
Maxim <110926014+MaximusPokeZ@users.noreply.github.com>
2024-11-09 20:38:28 +0300

    Revert "Заславцев Максим - лабораторная работа №1"

--- src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
@@ -1,38 +1,12 @@
 package ru.mai.lessons.rpks.impl;
 
-import lombok.extern.slf4j.Slf4j;
 import ru.mai.lessons.rpks.IFileReader;
-import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-@Slf4j
 public class FileReader implements IFileReader {
   @Override
-  public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException {
-    if (filePath == null || filePath.isEmpty()) {
-      throw new FilenameShouldNotBeEmptyException("Incorrect or empty path to file");
-    }
-
-    File file = new File(filePath);
-    if (!file.exists() || !file.canRead()) {
-      throw new FilenameShouldNotBeEmptyException("Cannot read file at this path: " + filePath);
-    }
-
-    List<String> lines = new ArrayList<>();
-
-    try (Scanner scanner = new Scanner(file)) {
-      while (scanner.hasNextLine()) {
-        lines.add(scanner.nextLine());
-      }
-    } catch (FileNotFoundException e) {
-      log.error("Error loading file: {}", e.getMessage(), e);
-      throw new FilenameShouldNotBeEmptyException("Error loading file: " + e.getMessage());
-    }
-    return lines;
+  public List<String> loadContent(String filePath) {
+    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.txt
   }
 }
