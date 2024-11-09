e9b03614c39ad36dceb73ca64a4bdeec45db18ff
wladislaw17 <wladislawpolezhaev@yandex.ru>
2024-11-09 13:55:57 +0300

    lab1

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -1,11 +1,26 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IConfigReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class ConfigReader implements IConfigReader {
-
   @Override
-  public String loadConfig(String configPath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.json
+  public String loadConfig(String configPath) throws FilenameShouldNotBeEmptyException {
+    if (configPath == null || configPath.isEmpty()) {
+      throw new FilenameShouldNotBeEmptyException("Path is empty or file is unavailable.");
+    }
+    try {
+      Path pathObject = Paths.get(configPath);
+      return Files.readString(pathObject);
+    } catch (IOException ex) {
+      System.err.println("Error while reading file: " + ex.getMessage());
+      ex.printStackTrace();
+      throw new FilenameShouldNotBeEmptyException("Error while reading file.");
+    }
   }
 }
--- src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/FileReader.java
@@ -1,12 +1,27 @@
 package ru.mai.lessons.rpks.impl;
 
 import ru.mai.lessons.rpks.IFileReader;
+import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
 
 import java.util.List;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class FileReader implements IFileReader {
   @Override
-  public List<String> loadContent(String filePath) {
-    return null; // написать код загрузки конфигураций сервиса проверки скобок из файла *.txt
+  public List<String> loadContent(String filePath) throws FilenameShouldNotBeEmptyException {
+    if (filePath == null || filePath.isEmpty()) {
+      throw new FilenameShouldNotBeEmptyException("Path is empty or file is unavailable.");
+    }
+    try {
+      Path pathObject = Paths.get(filePath);
+      return Files.readAllLines(pathObject);
+    } catch (IOException ex) {
+      System.err.println("Error while reading file: " + ex.getMessage());
+      ex.printStackTrace();
+      throw new IllegalArgumentException("Error while reading file: ", ex);
+    }
   }
 }
