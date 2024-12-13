2604dc43e0e9a37dbc45a593194904e26e664f62
Sergey Novikov <sergeylkd@bk.ru>
2024-12-13 17:26:43 +0300

    Done linter

--- src/main/java/ru/mai/lessons/rpks/impl/BracketsDetector.java
+++ src/main/java/ru/mai/lessons/rpks/impl/BracketsDetector.java
@@ -2,56 +2,54 @@ package ru.mai.lessons.rpks.impl;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.mai.lessons.rpks.IBracketsDetector;
 import ru.mai.lessons.rpks.result.ErrorLocationPoint;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.io.IOException;
 import java.util.*;
 
-
 public class BracketsDetector implements IBracketsDetector {
+
   private static final Logger log = LoggerFactory.getLogger(BracketsDetector.class);
 
   @Override
   public List<ErrorLocationPoint> check(String config, List<String> content) throws JsonParseException {
-    Map<Character, Character> brackets_template = ParseConfig(config);
+    Map<Character, Character> bracketsTemplate = parseConfig(config);
+    ProcessedBrackets processedBrackets = processBrackets(bracketsTemplate, content);
 
-    ProcessedBrackets processedBrackets = ProcessBrackets(brackets_template, content);
+    processBracketsDuplicates(bracketsTemplate, processedBrackets.brackets());
+    List<ErrorLocationPoint> errorLocationPoints = processErrorPosition(processedBrackets.brackets());
 
-    ProcessBracketsDuplicates(brackets_template, processedBrackets.brackets);
-
-    List<ErrorLocationPoint> errorLocationPoints = ProcessErrorPosition(brackets_template, processedBrackets.brackets);
-
-    List<ErrorLocationPoint> allErrorPoints = new ArrayList<>(processedBrackets.errorLocationPoints);
+    List<ErrorLocationPoint> allErrorPoints = new ArrayList<>(processedBrackets.errorLocationPoints());
     allErrorPoints.addAll(errorLocationPoints);
 
-    return  allErrorPoints;
+    return allErrorPoints;
   }
 
-  private void ProcessBracketsDuplicates(Map<Character, Character> brackets_template, Deque<Bracket> bracketsLeft) {
-    while (bracketsLeft.size() > 1 && SymmetricBracket(bracketsLeft.peek().Bracket, brackets_template)) {
+  private void processBracketsDuplicates(Map<Character, Character> bracketsTemplate, Deque<Bracket> bracketsLeft) {
+    while (bracketsLeft.size() > 1 && isSymmetricBracket(bracketsLeft.peek().bracket(), bracketsTemplate)) {
       bracketsLeft.pop();
     }
   }
 
-  private ProcessedBrackets ProcessBrackets(Map<Character, Character> template, List<String> content) {
+  private ProcessedBrackets processBrackets(Map<Character, Character> template, List<String> content) {
     Deque<Bracket> brackets = new ArrayDeque<>();
     List<ErrorLocationPoint> errorLocationPoints = new ArrayList<>();
 
     for (int lineNumber = 0; lineNumber < content.size(); lineNumber++) {
       String line = content.get(lineNumber);
-      ProcessBracketsDuplicates(template, brackets);
-      List<ErrorLocationPoint> errorLocationPointsNew = ProcessErrorPosition(template, brackets);
-      errorLocationPoints.addAll(errorLocationPointsNew);
+      processBracketsDuplicates(template, brackets);
+      errorLocationPoints.addAll(processErrorPosition(brackets));
+
       for (int charIndex = 0; charIndex < line.length(); charIndex++) {
         char currentChar = line.charAt(charIndex);
 
-        if (OperningBracket(currentChar, template)) {
-          if (SymmetricBracket(currentChar, template)) {
-            if (!brackets.isEmpty() && brackets.peek().Bracket == currentChar) {
+        if (isOpeningBracket(currentChar, template)) {
+          if (isSymmetricBracket(currentChar, template)) {
+            if (!brackets.isEmpty() && brackets.peek().bracket() == currentChar) {
               brackets.pop();
             } else {
               brackets.push(new Bracket(currentChar, lineNumber + 1, charIndex + 1));
@@ -59,10 +57,10 @@ public class BracketsDetector implements IBracketsDetector {
           } else {
             brackets.push(new Bracket(currentChar, lineNumber + 1, charIndex + 1));
           }
-        } else if (ClosingBracket(currentChar, template)) {
+        } else if (isClosingBracket(currentChar, template)) {
           if (!brackets.isEmpty()) {
             Bracket topBracket = brackets.peek();
-            if (!Objects.equals(template.get(topBracket.Bracket), currentChar)) {
+            if (!Objects.equals(template.get(topBracket.bracket()), currentChar)) {
               errorLocationPoints.add(new ErrorLocationPoint(lineNumber + 1, charIndex + 1));
             } else {
               brackets.pop();
@@ -76,28 +74,29 @@ public class BracketsDetector implements IBracketsDetector {
 
     return new ProcessedBrackets(brackets, errorLocationPoints);
   }
-  private boolean OperningBracket(char currentChar, Map<Character, Character> template) {
+
+  private boolean isOpeningBracket(char currentChar, Map<Character, Character> template) {
     return template.containsKey(currentChar);
   }
 
-  private boolean ClosingBracket(char currentChar, Map<Character, Character> template) {
+  private boolean isClosingBracket(char currentChar, Map<Character, Character> template) {
     return template.containsValue(currentChar);
   }
 
-  private boolean SymmetricBracket(char currentChar, Map<Character, Character> template) {
+  private boolean isSymmetricBracket(char currentChar, Map<Character, Character> template) {
     return template.get(currentChar) == currentChar && template.containsKey(currentChar);
   }
 
-  private  List<ErrorLocationPoint> ProcessErrorPosition(Map<Character, Character> template, Deque<Bracket> brackets) {
+  private List<ErrorLocationPoint> processErrorPosition(Deque<Bracket> brackets) {
     List<ErrorLocationPoint> errorLocationPoints = new ArrayList<>();
     if (!brackets.isEmpty()) {
-      errorLocationPoints.add(new ErrorLocationPoint(brackets.peek().NumberLine, brackets.pop().IndexInString));
+      errorLocationPoints.add(new ErrorLocationPoint(brackets.peek().numberLine(), brackets.pop().indexInString()));
     }
     brackets.clear();
     return errorLocationPoints;
   }
 
-  private Map<Character, Character> ParseConfig(String config) throws JsonParseException {
+  private Map<Character, Character> parseConfig(String config) throws JsonParseException {
     Map<Character, Character> result = new HashMap<>();
 
     try {
@@ -109,7 +108,7 @@ public class BracketsDetector implements IBracketsDetector {
         char rightBracket = node.get("right").asText().charAt(0);
         result.put(leftBracket, rightBracket);
       }
-    } catch (Exception e) {
+    } catch (IOException e) {
       log.error(e.getMessage());
       throw new JsonParseException("Invalid JSON configuration");
     }
@@ -117,6 +116,7 @@ public class BracketsDetector implements IBracketsDetector {
     return result;
   }
 
-  public record  Bracket(char Bracket, int NumberLine, int IndexInString) {}
-  public record  ProcessedBrackets(Deque<Bracket> brackets, List<ErrorLocationPoint> errorLocationPoints) {}
+  public record Bracket(char bracket, int numberLine, int indexInString) {}
+
+  public record ProcessedBrackets(Deque<Bracket> brackets, List<ErrorLocationPoint> errorLocationPoints) {}
 }
--- src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigReader.java
@@ -3,6 +3,7 @@ package ru.mai.lessons.rpks.impl;
 import ru.mai.lessons.rpks.IConfigReader;
 import ru.mai.lessons.rpks.exception.FilenameShouldNotBeEmptyException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,19 +14,26 @@ public class ConfigReader implements IConfigReader {
   @Override
   public String loadConfig(String configPath) throws FilenameShouldNotBeEmptyException {
     if (configPath == null || configPath.isEmpty()) {
-      throw new FilenameShouldNotBeEmptyException("The filepath is null");
+      throw new FilenameShouldNotBeEmptyException("The filepath is null or empty");
     }
 
     Path filepath = Paths.get("src/test/resources/" + configPath).toAbsolutePath();
 
     if (!Files.exists(filepath) || !Files.isRegularFile(filepath)) {
-      throw new FilenameShouldNotBeEmptyException("File not found " + filepath);
+      throw new FilenameShouldNotBeEmptyException("File not found: " + filepath);
     }
 
-    try {
-        return Files.readString(filepath);
+    StringBuilder configContent = new StringBuilder();
+
+    try (BufferedReader reader = Files.newBufferedReader(filepath)) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        configContent.append(line).append(System.lineSeparator());
+      }
     } catch (IOException e) {
-        throw new RuntimeException("Error while reading file",e);
+      throw new RuntimeException("Error while reading the file", e);
     }
+
+    return configContent.toString().trim();
   }
 }
